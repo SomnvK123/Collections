c1f5b31790bd3bf247c226deecd244f096cca00c
SVkdf092 <97460137+SomnvK123@users.noreply.github.com>
2025-04-23 16:26:56 +0700

    update

--- Entity/Word.java
+++ Entity/Word.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class Word {
     private String meaning;
-    private List<String> synonyms;
+    private Set<String> synonyms;
 
     public Word(String meaning) {
         this.meaning = meaning;
-        this.synonyms = new ArrayList<>();
+        this.synonyms = new LinkedHashSet<>();
     }
 
     public String getMeaning() {
         return meaning;
     }
 
-    public List<String> getSynonyms() {
-        return synonyms == null ? new ArrayList<>() : synonyms;
+    public Set<String> getSynonyms() {
+        return synonyms == null ? new LinkedHashSet<>() : synonyms;
     }
 
     public void addSynonym(String synonym) {
